ea93a7e6784f35835f31fd302daa90da64667278
0xjjpa <me@jjperezaguinaga.com>
2023-05-01 22:16:49 +0200

    feature: Open graph makes SEO easier and sharing visuallym more appealing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@fontsource/merriweather/700.css'
 import '@fontsource/open-sans/400.css'
 
+import { DefaultSeo } from 'next-seo';
 import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
@@ -8,9 +9,33 @@ import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <DefaultSeo
+        title="Passkeys.is"
+        description="Learn how to implement Passkeys and WebAuthn for secure authentication and authorization in your web applications."
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://passkeys.is/',
+          siteName: 'Passkeys.is',
+          images: [{
+            url: 'https://passkeys.is/images/passkeys.jpg',
+            width: 1200,
+            height: 630,
+            alt: 'Passkeys',
+          }],
+        }}
+        twitter={{
+          handle: '@0xjjpa',
+          site: '@0xjjpa',
+          cardType: 'summary_large_image',
+        }}
+      />
+
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   )
 }
 
