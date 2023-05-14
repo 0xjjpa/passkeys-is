12d7571162c0f29fba37787577648b88814f9e1b
Jose Aguinaga <me@jjperezaguinaga.com>
2023-05-14 04:07:47 +0000

    bug: Try again w/o the color to avoi losing icon (?)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default class Document extends NextDocument {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
